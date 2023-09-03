e82260747c94050bf08bee4b8979f30571abe7ea
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2023-09-03 14:25:29 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,12 @@ public class Main {
     public static void main(String[] args) {
         ToyShop shop = new ToyShop();
         
-        shop.addToy(1, "Медведь", 15, 30);
-        shop.addToy(2, "Заяц", 10, 25);
-        shop.addToy(3, "Жираф", 20, 45);
-        
+        shop.addToy(1, "Медведь", 15, 20);
+        shop.addToy(2, "Заяц", 10, 15);
+        shop.addToy(3, "Жираф", 20, 35);
+        shop.addToy(4, "Черепаха", 25,20);
+        shop.addToy(5, "Обезьяна", 30,10);
+            
         shop.setToyWeight(2, 30);
         
         shop.playTheGame();
